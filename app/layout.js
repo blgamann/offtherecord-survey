448ef0cfaf58619621db6a8826a28831aaae0d63
0xdagarn <sgnoo@naver.com>
2024-10-23 18:04:53 +0900

    Add twitter card

--- app/layout.js
+++ app/layout.js
@@ -23,6 +23,13 @@ export const metadata = {
     ],
     siteName: "오프더레코드",
   },
+  twitter: {
+    card: "summary_large_image",
+    title: "오프더레코드 - 방해꾼의 방",
+    description:
+      "솔직한 나를 방해하는 내면의 비판자 <방해꾼의 방>, 있는 그대로의 나를 드러내지 못하게 하는 내면의 방해꾼을 발견해 보세요.",
+    images: ["https://offtherecord.nicetomeetme.co.kr/opengraph-image.png"],
+  },
 };
 
 export default function RootLayout({ children }) {
